08941c8149ed64ea378365b6ce69f0f31d7b5c21
Thierry Martin <thierry.martin.2008@gadz.org>
2024-10-09 15:37:58 +0200

    creation api pennylane

--- api/pannylane.py
+++ api/pannylane.py
@@ -2,19 +2,18 @@ from ..utils.http_client import HttpClient
 from ..config import *
 
 
-class SAGEAPI:
-	# SAGE n'a pas encore vraiment d'API...
+class PennylaneAPI:
 	def __init__(self,
 				 sandbox: bool=True,
 				 client_id: str='',
 				 client_secret: str='',
 				 ):
 		self.sandbox = sandbox
-		self.client_id = client_id or SAGE_CLIENT_ID
-		self.client_secret = client_secret or SAGE_CLIENT_SECRET
-		self.base_url = SAGE_BASE_URL
+		self.client_id = client_id or PENNYLANE_CLIENT_ID
+		self.client_secret = client_secret or PENNYLANE_CLIENT_SECRET
+		self.base_url = PENNYLANE_BASE_URL
 		if self.sandbox:
-			self.base_url = SAGE_SANDBOX_BASE_URL
+			self.base_url = PENNYLANE_SANDBOX_BASE_URL
 		self.client = HttpClient(base_url=self.base_url, api_key='')
 
 	def get_token(self):
