3ec3a1e790faeb9764ef78cdf390e617245aa435
ThierryMartin <thierry.martin@cleverip.net>
2024-10-16 14:56:00 +0200

    facturx minimum

--- __init__.py
+++
@@ -1,4 +0,0 @@
-# from .api.chorus_pro import ChorusProAPI
-# from .api.sage import DPGFAPI
-# from .api.jedeclare import JeDeclareAPI
-# from .api.esker import EskerAPI
--- exceptions.py
+++
@@ -1,7 +0,0 @@
-class FacturationAPIError(Exception):
-	"""Erreur générique pour les erreurs liées à l'API de facturation électronique."""
-	pass
-
-class FactureNonTrouveeError(FacturationAPIError):
-	"""Exception lorsque la facture n'est pas trouvée."""
-	pass
