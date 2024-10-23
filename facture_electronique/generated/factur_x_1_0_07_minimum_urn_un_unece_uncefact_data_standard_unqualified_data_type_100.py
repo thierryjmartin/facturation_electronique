bec27058187ff79f874063a24c7ff654eff93606
ThierryMartin <thierry.martin@cleverip.net>
2024-10-23 11:57:19 +0200

    essai xsdata

---
+++ facture_electronique/generated/factur_x_1_0_07_minimum_urn_un_unece_uncefact_data_standard_unqualified_data_type_100.py
@@ -0,0 +1,81 @@
+from dataclasses import dataclass, field
+from decimal import Decimal
+from typing import Optional
+
+__NAMESPACE__ = "urn:un:unece:uncefact:data:standard:UnqualifiedDataType:100"
+
+
+@dataclass
+class AmountType:
+    value: Optional[Decimal] = field(
+        default=None,
+        metadata={
+            "required": True,
+        },
+    )
+    currency_id: Optional[str] = field(
+        default=None,
+        metadata={
+            "name": "currencyID",
+            "type": "Attribute",
+        },
+    )
+
+
+@dataclass
+class DateTimeType:
+    date_time_string: Optional["DateTimeType.DateTimeString"] = field(
+        default=None,
+        metadata={
+            "name": "DateTimeString",
+            "type": "Element",
+            "namespace": "urn:un:unece:uncefact:data:standard:UnqualifiedDataType:100",
+            "required": True,
+        },
+    )
+
+    @dataclass
+    class DateTimeString:
+        value: str = field(
+            default="",
+            metadata={
+                "required": True,
+            },
+        )
+        format: Optional[str] = field(
+            default=None,
+            metadata={
+                "type": "Attribute",
+                "required": True,
+            },
+        )
+
+
+@dataclass
+class Idtype:
+    class Meta:
+        name = "IDType"
+
+    value: str = field(
+        default="",
+        metadata={
+            "required": True,
+        },
+    )
+    scheme_id: Optional[str] = field(
+        default=None,
+        metadata={
+            "name": "schemeID",
+            "type": "Attribute",
+        },
+    )
+
+
+@dataclass
+class TextType:
+    value: str = field(
+        default="",
+        metadata={
+            "required": True,
+        },
+    )
